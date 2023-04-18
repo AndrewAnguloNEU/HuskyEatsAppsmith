be7b814bd2cc4be7c72dd750fddb4d2fe22df9e9
Andrew Angulo <angulo.an@northeastern.edu>
2023-04-18 19:34:04 +0000

    Added order cart

--- pages/RestuarantDetails/jsobjects/restaurant_detail_commands/restaurant_detail_commands.js
+++ pages/RestuarantDetails/jsobjects/restaurant_detail_commands/restaurant_detail_commands.js
@@ -5,5 +5,22 @@ export default {
 	filterProducts: () => {
 		const selectedMenuID = menu_select.selectedOptionValue
 		return get_products.data.filter(product => product.product_id === selectedMenuID)
+	},
+	addItemToOrder: () => {
+		const product = product_list?.selectedItem
+		if (product !== undefined) {
+			storeValue('orderProducts', [...(appsmith.store?.orderProducts || []), product])
+		}
+	},
+	get_subtotal: () => {
+		const orderProducts = appsmith.store?.orderProducts || []
+		return orderProducts.reduce((accumulator, currentValue) => (accumulator + parseFloat(currentValue.price)), 0)
+	},
+	submit_order: () =>  {
+		console.log("Products: ", appsmith.store?.orderProducts)
+		console.log("Subtotal: ", subtotal_input.inputText)
+		console.log("Tax: ", tax_input.inputText)
+		console.log("Fee: ", fee_input.inputText)
+		console.log("Tip: ", tip_input.inputText)
 	}
 }
