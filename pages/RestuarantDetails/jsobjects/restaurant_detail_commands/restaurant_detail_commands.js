6b7cde616c6d118f4690751bf7e2640af74e1442
Andrew Angulo <angulo.an@northeastern.edu>
2023-04-19 03:09:59 +0000

    Finished restaurant edit modal

--- pages/RestuarantDetails/jsobjects/restaurant_detail_commands/restaurant_detail_commands.js
+++ pages/RestuarantDetails/jsobjects/restaurant_detail_commands/restaurant_detail_commands.js
@@ -15,12 +15,5 @@ export default {
 	get_subtotal: () => {
 		const orderProducts = appsmith.store?.orderProducts || []
 		return orderProducts.reduce((accumulator, currentValue) => (accumulator + parseFloat(currentValue.price)), 0)
-	},
-	submit_order: () =>  {
-		console.log("Products: ", appsmith.store?.orderProducts)
-		console.log("Subtotal: ", subtotal_input.inputText)
-		console.log("Tax: ", tax_input.inputText)
-		console.log("Fee: ", fee_input.inputText)
-		console.log("Tip: ", tip_input.inputText)
 	}
 }
